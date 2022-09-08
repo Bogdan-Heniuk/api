91dcd9539bfa440886bc3b7004545828234c5081
Bohdan-Heniuk <genyukbogdan@gmail.com>
2022-09-08 20:06:47 +0300

    initial

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as AWS from 'aws-sdk'
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 }
+
+
+const s3 = new AWS.S3({
+    accessKeyId: process.env.S3_USER_ACCESS_KEY,
+    secretAccessKey: process.env.S3_SECRET,
+  })
+
+export default s3
+
 bootstrap();
